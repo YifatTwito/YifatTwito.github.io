ec975a166f0c00d9aa1c9549862191862a2c2105
Yifat Twito <55134372+YifatTwito@users.noreply.github.com>
2019-09-15 09:47:58 +0200

    kk
    jj

--- multipleFunction.js
+++
@@ -1,7 +0,0 @@
-function multipleFunction(){
-var num1 = document.getElementById("firstNumBox").Value;
-var num2 = document.getElementById("secondNumBox").Value;
-
-document.getElementById("Result").value = num1 * num2;
-
-}
--- plusFunction.js
+++
@@ -1,5 +0,0 @@
-function multipleFunction(){
-    var num1 = document.getElementById("firstNumBox").value;
-    var num2 = document.getElementById("secondNumBox").value;
-    
-    document.getElementById("Result").value = (num1 * num2);
