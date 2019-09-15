9c7259df5693e87ead0487d5fd165233c3457d72
Yifat Twito <55134372+YifatTwito@users.noreply.github.com>
2019-09-15 17:11:57 +0300

    Update multipleFunction.js

--- multipleFunction.js
+++ multipleFunction.js
@@ -1,6 +1,6 @@
 function multipleFunction(){
-var num1 = document.getElementById("firstNumBox").Value;
-var num2 = document.getElementById("secondNumBox").Value;
+var num1 = document.getElementById("firstNumBox").value;
+var num2 = document.getElementById("secondNumBox").value;
 
 document.getElementById("Result").value = num1 * num2;
 
