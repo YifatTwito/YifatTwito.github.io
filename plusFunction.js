1bb19bd9d7a84b94a034ab63bf5ba822326d2804
Yifat Twito <55134372+YifatTwito@users.noreply.github.com>
2019-09-15 09:32:04 +0200

    Update plusFunction.js

---
+++ plusFunction.js
@@ -0,0 +1,5 @@
+function multipleFunction(){
+    var num1 = document.getElementById("firstNumBox").value;
+    var num2 = document.getElementById("secondNumBox").value;
+    
+    document.getElementById("Result").value = (num1 * num2);
